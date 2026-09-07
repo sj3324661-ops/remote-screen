1045f3fc970bd88abf521a5d7954108142697194
sj3324661-ops <sj3324661@gmail.com>
2026-09-07 21:21:41 +0530

    Create RemoteScreenApplication class for app

---
+++ android/app/src/main/java/com/remotescreen/app/RemoteScreenApplication.java
@@ -0,0 +1,11 @@
+package com.remotescreen.app;
+
+import android.app.Application;
+
+public class RemoteScreenApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
